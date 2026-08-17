3f52ddcc6a76fd8fc378ec35895e837f591b505e
Rama <22110446@htu.edu.jo>
2026-08-17 14:02:13 +0300

    Add Material model

---
+++ BARAARama/Models/Material.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BARAARama.Models
+{
+    public class Material
+    {
+        [Key]
+        public int MaterialId { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string MaterialName { get; set; } = "";
+    }
+}
