9d74d26de45af53e0912f9ee8e195f67157ff1f0
Rama <22110446@htu.edu.jo>
2026-08-19 11:42:14 +0300

    Add cashier feature and update inventory

---
+++ BARAARama/Migrations/20260819083646_FixCashierWithdrawalRelationships.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BARAARama.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixCashierWithdrawalRelationships : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_CashierWithdrawals_MaterialId",
+                table: "CashierWithdrawals",
+                column: "MaterialId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CashierWithdrawals_Materials_MaterialId",
+                table: "CashierWithdrawals",
+                column: "MaterialId",
+                principalTable: "Materials",
+                principalColumn: "MaterialId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CashierWithdrawals_Materials_MaterialId",
+                table: "CashierWithdrawals");
+
+            migrationBuilder.DropIndex(
+                name: "IX_CashierWithdrawals_MaterialId",
+                table: "CashierWithdrawals");
+        }
+    }
+}
--- BARAARama/Models/Inventory.cs
+++ BARAARama/Models/Inventory.cs
@@ -1,15 +1,24 @@
-﻿namespace BARAARama.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BARAARama.Models
 {
     public class Inventory
     {
+        [Key]
         public int InventoryId { get; set; }
 
+        [Required]
         public int MaterialId { get; set; }
 
+        [ForeignKey(nameof(MaterialId))]
         public Material Material { get; set; } = null!;
 
+        [Range(0, int.MaxValue)]
         public int Quantity { get; set; }
 
+        [Column(TypeName = "decimal(18,2)")]
+        [Range(0.01, 1000000)]
         public decimal Price { get; set; }
     }
 }
